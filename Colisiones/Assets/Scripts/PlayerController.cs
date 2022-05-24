6581f274012fff53e262adaff571c731ab6902e8
LaraGenovese <93852155+LaraGenovese@users.noreply.github.com>
2022-05-24 10:40:47 -0300

    cube game

---
+++ Colisiones/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position = new Vector3(3.43f, 1.6f, -3.02f);  
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position = new Vector3(3.43f, 0.5f, -3.02f);
+        }
+    }
+}
